b5563aef5bbd67d3710fb93647f78939f1633479
Kyle Escobar <kslrtips@gmail.com>
2020-10-11 01:39:20 -0400

    Finish application manifest file model

--- src/main/kotlin/org/spectral/launcher/manifest/AppManifest.kt
+++ src/main/kotlin/org/spectral/launcher/manifest/AppManifest.kt
@@ -17,8 +17,14 @@
 
 package org.spectral.launcher.manifest
 
+import org.spectral.launcher.util.DataDirectory
+import org.spectral.launcher.util.Platform
+import java.io.File
 import java.net.URI
+import java.nio.file.Path
+import javax.xml.bind.JAXB
 import javax.xml.bind.annotation.XmlAttribute
+import javax.xml.bind.annotation.XmlElement
 import javax.xml.bind.annotation.XmlRootElement
 
 /**
@@ -26,7 +32,7 @@ import javax.xml.bind.annotation.XmlRootElement
  * version of the client as well as any libraries.
  */
 @XmlRootElement(name = "Application")
-class AppManifest {
+class AppManifest private constructor() {
 
     /**
      * The timestamp this manifest was created at.
@@ -46,5 +52,83 @@ class AppManifest {
     @XmlAttribute(name = "main")
     lateinit var mainClass: String
 
+    /**
+     * The application files this manifest in for.
+     */
+    @XmlElement
+    val files = mutableListOf<AppFile>()
 
+    /**
+     * The version of this manifest or application.
+     */
+    @XmlElement
+    lateinit var version: String
+
+    /**
+     * The path where the application files are stored at on the local
+     * system running this launcher process.
+     */
+    @XmlElement
+    lateinit var cacheDir: String
+
+    /**
+     * Gets the cache directory respective to the current running
+     * platform operating system.
+     *
+     * @return Path
+     */
+    fun resolveCacheDir(): Path {
+        /*
+         * The spectral data directory.
+         */
+        val dataDirPath = DataDirectory.forPlatform(Platform.current())
+
+        /*
+         * Return the resolved cache directory path.
+         */
+        return dataDirPath.resolve(cacheDir)
+    }
+
+    /**
+     * Gets whether this app manifest is a newer version that a provided
+     * [other] manifest object.
+     *
+     * @param other AppManifest
+     * @return Boolean
+     */
+    fun isNewerThan(other: AppManifest): Boolean {
+        return ts > other.ts
+    }
+
+    companion object {
+        /**
+         * Creates a application manifest object from a provided
+         * URI. This can either be a remote server or a local file.
+         *
+         * @param uri URI
+         * @return AppManifest
+         */
+        fun load(uri: URI): AppManifest {
+            /*
+             * If the provided URI is a local file. We can just
+             * directly unmarshal the manifest file.
+             */
+            if(uri.scheme == "file") {
+               return JAXB.unmarshal(File(uri.path), AppManifest::class.java)
+            }
+
+            /*
+             * Otherwise the uri must be a remote file. We need to make
+             * a connection to the remote URI server to download the manfiest file.
+             */
+            val connection = uri.toURL().openConnection()
+
+            /*
+             * Stream / download the bytes from the remote manifest file.
+             */
+            connection.getInputStream().use { reader ->
+                return JAXB.unmarshal(reader, AppManifest::class.java)
+            }
+        }
+    }
 }
